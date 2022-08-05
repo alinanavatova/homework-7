34b0d26e82df1c39d9fd5acb93fab077d83b9ed0
alinanavatova <109083330+alinanavatova@users.noreply.github.com>
2022-08-05 17:11:06 +0300

    Update and rename src/com/company/Main.java to источник/ком/Компания/Main.java

--- источник/ком/Компания/Main.java
+++ источник/ком/Компания/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
         Hero[] heroes = {magic, medic, warrior};
         for (int i = 0; i < heroes.length; i++) {
-            heroes[i].applySuperAbility("heroes' superAbilityType");
+            heroes[i].applySuperAbility(heroes[i].getSuperAbilityType());
         }
     }
 }
